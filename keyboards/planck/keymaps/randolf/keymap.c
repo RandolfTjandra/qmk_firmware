98ccc4934410c2398ab517e8662a7995f37a3775
Randolf Tjandra <randolftjandra@gmail.com>
2023-10-21 15:31:33 -0700

    Fix reset -> QK_BOOT
    Reviewer: none
    Issue: none

--- keyboards/planck/keymaps/randolf/keymap.c
+++ keyboards/planck/keymaps/randolf/keymap.c
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_planck_grid( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-  _______, _______, MU_NEXT,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, _______,  _______, RESET, \
+  _______, _______, MU_NEXT,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, _______,  _______, QK_BOOT, \
   _______, AU_PREV, AU_NEXT,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  RGB_TOG, RGB_RMOD, RGB_MOD, RGB_HUD, RGB_HUI, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 )
