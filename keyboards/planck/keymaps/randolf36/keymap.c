dacb1a99bd10635f0f192ac0682dd6727560da63
Randolf Tjandra <randolftjandra@gmail.com>
2023-10-21 14:45:00 -0700

    trying to make new layout?
    Reviewer: none
    Issue: none

--- keyboards/planck/keymaps/randolf36/keymap.c
+++ keyboards/planck/keymaps/randolf36/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_grid( \
   QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
-  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+  KC_TAB,  KC_A,    KC_S,    KC_D,    LT(_LOWER, KC_F),    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, RSFT_T(KC_SLSH), \
   KC_LCTL, LCTL(LSFT(KC_LALT)), KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT  \
 ),
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_planck_grid( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
   _______, _______, _______, AU_ON  , AU_OFF , AG_NORM, AG_SWAP, QWERTY,  _______, _______,  _______, RESET, \
-  _______, _______, _______, _______, _______, _______, _______,  RGB_TOG, RGB_RMOD, RGB_MOD, RGB_HUD, RGB_HUI, \
+  _______, _______, _______, _______, _______, _______, _______, BL_TOGG, RGB_RMOD, RGB_MOD, RGB_HUD, RGB_HUI, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 )
 
---
+++ keyboards/planck/keymaps/randolf36/readme.md
@@ -0,0 +1,3 @@
+# Randolf36
+
+I attempt to condense my layout to 36 keys
