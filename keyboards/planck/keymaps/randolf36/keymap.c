67285ad8eabf930ac505e20bcc1d2a2d62e983a2
Randolf Tjandra <randolftjandra@gmail.com>
2023-10-22 20:38:40 -0700

    Implement changes in readme
    Reviewer: none
    Issue: none

--- keyboards/planck/keymaps/randolf36/keymap.c
+++ keyboards/planck/keymaps/randolf36/keymap.c
@@ -51,10 +51,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid( \
-  QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
-  KC_TAB,  KC_A,    KC_S,    KC_D,    LT(_LOWER, KC_F),    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, RSFT_T(KC_SLSH), \
-  KC_LCTL, LCTL(LSFT(KC_LALT)), KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT  \
+  QK_GESC, KC_Q,                KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,  \
+  KC_TAB,  LCTL_T(KC_A),        KC_S,    LGUI_T(KC_D),    KC_F,    KC_G,    KC_H,    KC_J, RGUI_T(KC_K), KC_L,    RCTL_T(KC_SCLN), _______, \
+  KC_LSFT, LSFT_T(KC_Z),        KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_QUOT), RSFT_T(KC_SLSH), \
+  KC_LCTL, LCTL(LSFT(KC_LALT)), KC_LALT, KC_LGUI, KC_TAB, LT(_LOWER, KC_BSPC),  LT(_RAISE, KC_SPC),  KC_ENT,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT  \
 ),
 
 /* Lower
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid( \
   KC_GRV,  KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, KC_PGUP, KC_UP,   KC_PGDN,  _______, KC_DEL, \
   _______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, \
-  KC_LSFT, _______, _______, _______, _______, _______, KC_END, _______, _______, _______, _______, _______, \
+  KC_LSFT, _______, _______, _______, _______, _______, KC_END , _______, _______, _______, _______, _______, \
   KC_LCTL, _______, KC_LALT, KC_LGUI, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid( \
   KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_DEL, \
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS, _______, \
+  _______, _______, _______, _______, _______, _______, _______, KC_SLSH, KC_LBRC, KC_RBRC, KC_BSLS, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
@@ -124,8 +124,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_planck_grid( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-  _______, _______, _______, AU_ON  , AU_OFF , AG_NORM, AG_SWAP, QWERTY,  _______, _______,  _______, RESET, \
-  _______, _______, _______, _______, _______, _______, _______, BL_TOGG, RGB_RMOD, RGB_MOD, RGB_HUD, RGB_HUI, \
+  _______, _______, MU_NEXT,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, _______,  _______, QK_BOOT, \
+  _______, AU_PREV, AU_NEXT,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  RGB_TOG, RGB_RMOD, RGB_MOD, RGB_HUD, RGB_HUI, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 )
 
@@ -136,9 +136,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   float plover_gb_song[][2]  = SONG(PLOVER_GOODBYE_SOUND);
 #endif
 
-/* uint32_t layer_state_set_user(uint32_t state) { */
-/*   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST); */
-/* } */
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    print("debugging tapping term\n");
+    printf("%d", TAPPING_TERM);
+    switch (keycode) {
+        case SFT_T(KC_SPC):
+            return TAPPING_TERM + 1250;
+        default:
+            return TAPPING_TERM;
+    }
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
--- keyboards/planck/keymaps/randolf36/readme.md
+++ keyboards/planck/keymaps/randolf36/readme.md
@@ -1,3 +1,23 @@
 # Randolf36
 
-I attempt to condense my layout to 36 keys
+## cmd
+
+make planck/rev6_drop:randolf36
+
+Changes so far
+
+-   Put lower on F
+-   Put backspace on left “space” so I can get rid of the top right one
+-   Removed lower on F, moved layers to backspace and space
+-   I put shift on Z and on A on hold layers
+-   I put / on the raise layer where m is
+-   Remove the original backspace
+-   Put return where RAISE used to be
+-   Remove original return
+-   Hold a for ctl and tap for a
+-   Put tab where lower used to be
+-   Adjust the behavior of the mod tapping so I can type at normal speed
+
+<!-- function foobar(param: string) { -->
+<!-- console.log(param); -->
+<!-- } -->
